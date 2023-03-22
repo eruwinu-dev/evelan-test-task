1e699382fcc4a45b5d7c68d03c52e42df8bf9587
Erwin Joshua Encisa <erwin.joshua.encisa@eee.upd.edu.ph>
2023-03-22 22:19:28 +0800

    Changed user card UI

--- src/components/UserList/UserItem/index.tsx
+++ src/components/UserList/UserItem/index.tsx
@@ -1,6 +1,6 @@
 import { User } from "@/types/user"
 import Image from "next/image"
-import React from "react"
+import React, { memo } from "react"
 
 type Props = {
 	user: User
@@ -10,23 +10,27 @@ const UserItem = ({ user }: Props) => {
 	const name = `${user.first_name} ${user.last_name}`
 	return (
 		<div className="user-item">
-			<div className="user-image">
-				<Image
-					src={user.avatar}
-					alt={name}
-					fill
-					className="rounded-full object-cover p-1"
-					sizes="(max-width: 768px) 75px,
+			<div>
+				<figure className="user-image">
+					<Image
+						src={user.avatar}
+						alt={name}
+						fill
+						className="rounded-full object-cover p-1"
+						sizes="(max-width: 768px) 75px,
 							(max-width: 1200px) 100px,
 							125px"
-				/>
+					/>
+				</figure>
 			</div>
-			<span>{user.id}</span>
-			<h2>{user.email}</h2>
-			<h3>{name}</h3>
+			<div className="user-desc">
+				<h2>{name}</h2>
+				<h3>{user.email}</h3>
+				<label htmlFor={user.email}>{user.id}</label>
+			</div>
 		</div>
 	)
 }
 
-export default UserItem
+export default memo(UserItem)
 
